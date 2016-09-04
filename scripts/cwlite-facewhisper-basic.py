353d0eb25b4ccb4140f07fd846599b508c394706
Micah Elizabeth Scott <micah@misc.name>
2016-09-04 16:52:06 -0700

    Looking for longer than usual responses

--- scripts/cwlite-facewhisper-basic.py
+++ scripts/cwlite-facewhisper-basic.py
@@ -69,7 +69,7 @@ class UserScript(UserScriptBase):
             ['Glitch Module', 'Ext Trigger Offset', 2400],
 
             ['Glitch Explorer', 'Normal Response', u's.find("code 5 len 34 9 2 22 0 1 1 0 80 28 9 4 0 0 1 3 1 2 0 9 21") >= 0'],
-
+            ['Glitch Explorer', 'Successful Response', u"int(s.split('len ')[-1].split()[0]) > 34"],
             ]:
             self.api.setParameter(cmd)
 
